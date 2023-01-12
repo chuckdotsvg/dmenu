b761d40b1ae0c636dc39baf8c96ed7519747054d
Carlo Aspren Gines Lara <101257700+PinoTheCreator@users.noreply.github.com>
2023-01-13 00:13:01 +0100

    set readme and changed colors

--- config.h
+++ config.h
@@ -2,21 +2,21 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom */
-static int fuzzy = 0;                       /* -F  option; if 0, dmenu doesn't use fuzzy matching */
-static int center = 0;                      /* -c  option; if 0, dmenu won't be centered on the screen */
+static int fuzzy = 1;                       /* -F  option; if 0, dmenu doesn't use fuzzy matching */
+static int center = 1;                      /* -c  option; if 0, dmenu won't be centered on the screen */
 static int min_width = 500;                 /* minimum width when centered */
 static const int vertpad = 0;              /* vertical padding of bar */
 static const int sidepad = 0;              /* horizontal padding of bar */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] =
-{
-  "Fira Code:size=11",
-  "Noto Color Emoji:size=11",
-	"monospace:size=10"
+static const char *fonts[] = {
+    "SF Pro Text:size=15",
+    "Fira Code:size=11",
+    "Noto Color Emoji:size=11",
+    "monospace:size=10"
 };
 static const char *prompt      = "🔍 Search:";      /* -p  option; prompt to the left of input field */
 
-#include "themes/gruvbox-dark.h"
+#include "themes/tomorrow-night.h"
 
 //static
 //const
@@ -31,7 +31,7 @@ static const char *prompt      = "🔍 Search:";      /* -p  option; prompt to t
 //	[SchemeHp]   = { "#bbbbbb", "#333333" },
 //};
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 10;
 static unsigned int maxhist    = 15;
 static int histnodup           = 1;	/* if 0, record repeated histories */
 
@@ -42,7 +42,7 @@ static int histnodup           = 1;	/* if 0, record repeated histories */
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 0;
+static unsigned int border_width = 2;
 
 /*
  * Use prefix matching by default; can be inverted with the -x flag.
--- themes/tomorrow-night.h
+++ themes/tomorrow-night.h
@@ -1,10 +1,10 @@
 static const char *colors[][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c5c8c6", "#1d1f21" },
-	[SchemeSel] = { "#000000", "#b77ee0" },
-	[SchemeOut] = { "#000000", "#54ced6" },
-  [SchemeBorder] = { "#000000", "#b77ee0" },
-	[SchemeSelHighlight] = { "#9ec400", "#000000" },
-	[SchemeNormHighlight] = { "#9ec400", "#000000" },
-	[SchemeHp] = { "#c5c8c6", "#000000" },
+    /*     fg         bg       */
+    [SchemeNorm] = { "#c5c8c6", "#1a1b26" },
+	[SchemeSel] = { "#1a1b26", "#7aa2f7" },
+    [SchemeOut] = { "#1a1b26", "#54ced6" },
+    [SchemeBorder] = { "#1a1b26", "#7aa2f7" },
+    [SchemeSelHighlight] = { "#9ec400", "#1a1b26" },
+	[SchemeNormHighlight] = { "#9ec400", "#1a1b26" },
+    [SchemeHp] = { "#c5c8c6", "#1a1b26" },
 };
